072bbeea4a743dc48d354102d54b7fe4d1b60a51
josedomenech <m.jose.domenech@gmail.com>
2024-04-21 19:47:58 -0300

    scripts para hacer ajustes modificados

--- grafico-ajustes-qg.py
+++ grafico-ajustes-qg.py
@@ -20,14 +20,14 @@ pwd = 'C:/Users/mariajose/Desktop/archivos-para-ccad/simulaciones-Durga/simulaci
 #data2r = np.loadtxt(pwd+'retorno_09_1000_10E6.txt')
 #data3r = np.loadtxt(pwd+'retorno_09_100_10E6.txt')
 
-#data0 = np.loadtxt(pwd+'delta_00001_10000_10E6_RECONSTRUIDO.txt')
+data0 = np.loadtxt(pwd+'delta_00001_10000_10E6_RECONSTRUIDO.txt')
 #data1 = np.loadtxt(pwd+'delta_00002_5000_10E6_RECONSTRUIDO.txt')
-data2 = np.loadtxt(pwd+'delta_0001_1000_10E6.txt')
+#data2 = np.loadtxt(pwd+'delta_0001_1000_10E6.txt')
 #data3 = np.loadtxt(pwd+'delta_001_100_10E6.txt')
 
-#data0r = np.loadtxt(pwd+'retorno_00001_10000_10E6_RECONSTRUIDO.txt')
+data0r = np.loadtxt(pwd+'retorno_00001_10000_10E6_RECONSTRUIDO.txt')
 #data1r = np.loadtxt(pwd+'retorno_00002_5000_10E6_RECONSTRUIDO.txt')
-data2r = np.loadtxt(pwd+'retorno_0001_1000_10E6.txt')
+#data2r = np.loadtxt(pwd+'retorno_0001_1000_10E6.txt')
 #data3r = np.loadtxt(pwd+'retorno_001_100_10E6.txt')
 
 
@@ -48,30 +48,33 @@ label3 = '$N=10^2$'
 
 
 #------------------- configuración ----------------
-datadelta=data2                             #  (100: 3, 1000: 2 , 5000: 1, 10000: 0)
-intervalo1=[0,3000]  # Intervalo ajuste lineal (100: [25,100], 1000: [25,1000] , 5000: [30,4500], 10000: [30,9500])
+datadelta=data0                             #  (100: 3, 1000: 2 , 5000: 1, 10000: 0)
+intervalo1=[2,3000]  # Intervalo ajuste lineal (100: [25,100], 1000: [25,1000] , 5000: [30,4500], 10000: [30,9500])
 lim_escala=100000
 
-dataretorno = data2r
-lim_grafico =  [-40000,40000]                #  (100: [-1500,1500], 1000:  [-5200,5200], 5000: [-16000,16000], 10000: [-19000,19000]) 
-lim_graficoZOOM=[-800,800]
+dataretorno = data0r
+lim_grafico =  [-15000,15000]                #  (100: [-1500,1500], 1000:  [-5200,5200], 5000: [-16000,16000], 10000: [-19000,19000]) 
+lim_graficoZOOM=[-2000,2000]
 tau = 1.5                                  #  (100: 1.51, 1000: 1.44, 5000: 1.44, 10000: 1.46)  
-initial_guess = [27,0.000005]    #aq, a1
-int_graficado =[-25000,25000]    #hasta donde grafico curva ajustada #  (100: [-510,510], 1000:  [-4000,4000], 5000: [-12000,12000], 10000: [-20000,20000])                    
-int_ajuste = [-1000,1000]      #en este int hago el ajuste para que el zoom se vea bien (100:[-370,370], 1000:  [-3800,3800], 5000: [-9950,9950], 10000: [-9950,9950] )
+initial_guess1 = 0.01  #beta
+initial_guess = [27,0.000005]     #aq, a1
+int_graficado =[-35000,35000]    #hasta donde grafico curva ajustada #  (100: [-510,510], 1000:  [-4000,4000], 5000: [-12000,12000], 10000: [-20000,20000])                    
+int_ajusteqg = [-25000,25000]      #en este int hago el ajuste para que el zoom se vea bien (100:[-370,370], 1000:  [-3800,3800], 5000: [-9950,9950], 10000: [-9950,9950] )
+int_ajuste = [-9000,9000]
 int_excluido = [0,0]
 
 ig = False
 qg = True
+f = False
 
-color=color2
-marker=marker2
-label=label2
+color=color0
+marker=marker0
+label=label0
 
 # Ruta para guardar graficos
 pwdgraf = 'C:/Users/mariajose/Desktop/graficos_tf/'
-save_fig = False
-nombre_archivo = 'AJUSTES_0001_norm-val-max_1000'
+save_fig = True
+nombre_archivo = 'AJUSTES_0001_norm-val-max_10000'
 
 #0.9
 # tau                  100: 1.51,     1000: 1.44,     5000: 1.44,     10000: 1.46
@@ -95,6 +98,10 @@ def FUNC_log(x,aq,a1): # Cambiar valores de 1/(1-q) y de (q-1) según cada q
 def qgaussian(x,aq):
     
     return ((1-(1-2.33)*aq*(x**2))**(1/(1-2.33)))
+
+def qgaussian_log(x,aq):
+    
+    return (1/(1-2.33))*np.log(1-(1-2.33)*aq*(x**2))
     
 
 def separar_par_impar(data):
@@ -104,7 +111,7 @@ def separar_par_impar(data):
     
     return data_pares, data_impares
 
-def grafico(datadelta,lim_escala,intervalo1,   dataretorno,lim_grafico,tau,int_ajuste,int_graficado,initial_guess,ig,qg,int_excluido,  color,marker,label,pwdgraf,nombre_archivo,save_fig):
+def grafico(datadelta,lim_escala,intervalo1,   dataretorno,lim_grafico,tau,int_ajusteqg,int_ajuste,int_graficado,initial_guess1,initial_guess,ig,qg,f,int_excluido,  color,marker,label,pwdgraf,nombre_archivo,save_fig):
     #NORMALIZADO AL VAL MAX
 
     fig = plt.figure(figsize = [10.5, 3.9],layout='tight' )
@@ -113,12 +120,17 @@ def grafico(datadelta,lim_escala,intervalo1,   dataretorno,lim_grafico,tau,int_a
     #########################
     ax1 = fig.add_subplot(121)
     
-    
+
     #====================================
     intervalo1=intervalo1 # Intervalo para hacer el ajuste lineal
     lim_escala=lim_escala # Límite escala y grafico
     
+    # Filtrar los valores mayores a lim_escala
+    datadelta = datadelta[datadelta <= lim_escala]
+
+    #binsdelta1 = np.linspace(1.5, np.amax(datadelta)+0.5, int(np.amax(datadelta)/2))
     binsdelta1 = np.linspace(1.5, np.amax(datadelta)+0.5, int(np.amax(datadelta))) 
+    
     y1, bin_edges = np.histogram(datadelta, bins=binsdelta1)
     x1 = bin_edges[1:]
     x1 = x1.astype(int)
@@ -131,6 +143,7 @@ def grafico(datadelta,lim_escala,intervalo1,   dataretorno,lim_grafico,tau,int_a
     ax1.scatter(x1, y1, marker=marker, facecolor='none', edgecolor=color, s=10, label=label)
     #-------------------------------- 
     #AJUSTE
+
     x=np.log10(x1)
     y=np.log10(y1)
     xcasi = x[x >= np.log10(intervalo1[0])]         # Cota inferior
@@ -163,10 +176,10 @@ def grafico(datadelta,lim_escala,intervalo1,   dataretorno,lim_grafico,tau,int_a
     print('q: ', round(q,3))
 
     # Grafico datos
-    binsretorno = np.linspace(lim_grafico[0]-0.5,lim_grafico[1]+0.5,-lim_grafico[0]+lim_grafico[1]+2)
+    binsretorno = np.linspace(lim_grafico[0]-0.5,lim_grafico[1]+0.5,int(((-lim_grafico[0]+lim_grafico[1])/2)+2))
     #binsretorno = np.linspace(lim_grafico[0]-0.5,lim_grafico[1]+0.5,int((-lim_grafico[0]+lim_grafico[1]+2)/10))
     y_tot, bin_edges_tot = np.histogram(dataretorno, binsretorno)
-    x_tot = np.linspace(lim_grafico[0],lim_grafico[1],-lim_grafico[0]+lim_grafico[1]+1).astype(int)
+    x_tot = np.linspace(lim_grafico[0],lim_grafico[1],int((-lim_grafico[0]+lim_grafico[1])/2+1)).astype(int)
     #x_tot = np.linspace(lim_grafico[0],lim_grafico[1],int((-lim_grafico[0]+lim_grafico[1]+2)/10)-1).astype(int)
     p00_tot = np.amax(y_tot)
     mask = y_tot != 0   # Máscara booleana que quita los puntos del histograma que valdrían cero 
@@ -179,56 +192,96 @@ def grafico(datadelta,lim_escala,intervalo1,   dataretorno,lim_grafico,tau,int_a
     #-------------------------------- 
     # Ajustar los datos
 
-    dataretorno = separar_par_impar(dataretorno)[0]  # El ajuste lo hago en la rama par  
-    binsretorno = np.linspace(int_ajuste[0]-0.5,int_ajuste[1]+0.5,-int_ajuste[0]+int_ajuste[1]+2)
-    #binsretorno = np.linspace(int_ajuste[0]-0.5,int_ajuste[1]+0.5,int((-int_ajuste[0]+int_ajuste[1]+2)/10))
-    y, bin_edges_tot = np.histogram(dataretorno, binsretorno)
-    x = np.linspace(int_ajuste[0],int_ajuste[1],-int_ajuste[0]+int_ajuste[1]+1).astype(int)
-    #x = np.linspace(int_ajuste[0],int_ajuste[1],int((-int_ajuste[0]+int_ajuste[1]+2)/10)-1).astype(int)
-    p00 = np.amax(y)
-    mask = y != 0   # mascara booleana que quita los puntos del histograma que valdrían cero 
-    x = x[mask]
-    y = y[mask] # (joden al calcularles el log pq log(0) es menos inf)
-    y = y/p00
-    y = np.log(y)
-                                                                          
-    # Le quitamos a x los valores entre -30,30 para que el pico donde se abre no lo tome en el ajuste
-    x_filtrado = x[(x <= int_excluido[0]) | (x >= int_excluido[1])]
-    y_filtrado = y[(x <= int_excluido[0]) | (x >= int_excluido[1])]
-
-    popt, pcov = curve_fit(FUNC_log, x_filtrado, y_filtrado, p0=initial_guess, bounds=([0,0], [np.inf,np.inf]))
-
-    print(label)
-    print('aq: ',round(popt[0],3), 'sin redondear: ',popt[0])
-    print('a1: ',round(popt[1],10), 'sin redondear: ',popt[1])
-
-    x_filtrado_mayor = x_filtrado[x_filtrado >= 0]
-    x_filtrado_menor = x_filtrado[x_filtrado <= 0]
-    ax2.plot(x_filtrado_mayor, 10**(FUNC_log(x_filtrado_mayor, *popt)), color='black',label=f'Ajuste $f(x)$, $q = {q:.2f}$', linewidth=0.8)
-    ax2.plot(x_filtrado_menor, 10**(FUNC_log(x_filtrado_menor, *popt)), color='black', linewidth=0.8)
-    #ax2.plot(x, 10**(FUNC_log(x, *popt)), color='black',label=f'Ajuste $f(x)$, $q = {q:.2f}$', linewidth=0.8)
+    popt1=0
+    popt=0
+    if qg == True:
+        dataretorno = separar_par_impar(dataretorno)[0]  # El ajuste lo hago en la rama par  
+        binsretorno = np.linspace(int_ajusteqg[0]-0.5,int_ajusteqg[1]+0.5,-int_ajusteqg[0]+int_ajusteqg[1]+2)
+        #binsretorno = np.linspace(int_ajuste[0]-0.5,int_ajuste[1]+0.5,int((-int_ajuste[0]+int_ajuste[1]+2)/10))
+        y, bin_edges_tot = np.histogram(dataretorno, binsretorno)
+        x = np.linspace(int_ajusteqg[0],int_ajusteqg[1],-int_ajusteqg[0]+int_ajusteqg[1]+1).astype(int)
+        #x = np.linspace(int_ajuste[0],int_ajuste[1],int((-int_ajuste[0]+int_ajuste[1]+2)/10)-1).astype(int)
+        p00 = np.amax(y)
+        mask = y != 0   # mascara booleana que quita los puntos del histograma que valdrían cero 
+        x = x[mask]
+        y = y[mask] # (joden al calcularles el log pq log(0) es menos inf)
+        y = y/p00
+        y = np.log(y)
+                                                                              
+        # Le quitamos a x los valores entre -30,30 para que el pico donde se abre no lo tome en el ajuste
+        #x_filtrado = x[(x <= int_excluido[0]) | (x >= int_excluido[1])]
+        #y_filtrado = y[(x <= int_excluido[0]) | (x >= int_excluido[1])]
+        popt1, pcov = curve_fit(qgaussian_log, x, y, p0=initial_guess1)#, bounds=([0,0], [np.inf,np.inf]))
+        print(label)
+        print('beta: ',round(popt1[0],3), 'sin redondear: ',popt1)
     
-    x_proy = np.linspace(int_graficado[0],int_graficado[1],-int_graficado[0]+int_graficado[1]+1).astype(int)
-    x_proy_mayor = x_proy[x_proy >= x_filtrado[-1]]
-    x_proy_menor = x_proy[x_proy <= x_filtrado[0]]
-    ax2.plot(x_proy_mayor, 10**(FUNC_log(x_proy_mayor, *popt)), color='black', linestyle='--' , linewidth=0.8)#label='Proyección'
-    ax2.plot(x_proy_menor, 10**(FUNC_log(x_proy_menor, *popt)), color='black', linestyle='--' , linewidth=0.8)#label='Proyección'
-    #ax2.plot(x_proy, 10**(FUNC_log(x_proy, *popt)), color='black', linestyle='--' , linewidth=0.8)#label='Proyección'
+        #x_filtrado_mayor = x_filtrado[x_filtrado >= 0]
+        #x_filtrado_menor = x_filtrado[x_filtrado <= 0]
+        #######ax2.plot(x, 10**(qgaussian_log(x, popt1[0])), color='red', linewidth=0.8)
+        #ax2.plot(x_filtrado_mayor, 10**(qgaussian_log(x_filtrado_mayor, popt1[0])), color='red',label=f'Ajuste $f(x)$, $q = {q:.2f}$', linewidth=0.8)
+        #ax2.plot(x_filtrado_menor, 10**(qgaussian_log(x_filtrado_menor, popt1[0])), color='red', linewidth=0.8)
+        
+        x_proy = np.linspace(int_graficado[0],int_graficado[1],-int_graficado[0]+int_graficado[1]+1).astype(int)
+        #x_proy_mayor = x_proy[x_proy >= x_filtrado[-1]]
+        #x_proy_menor = x_proy[x_proy <= x_filtrado[0]]
+        
+        #ax2.plot(x_proy_mayor, 10**(qgaussian_log(x_proy_mayor, popt1[0])), color='black', linestyle='--' , linewidth=0.8)#label='Proyección'
+        #ax2.plot(x_proy_menor, 10**(qgaussian_log(x_proy, popt1[0])), color='black', linestyle='--' , linewidth=0.8)#label='Proyección'
+        ax2.plot(x_proy, 10**(qgaussian_log(x_proy, popt1[0])), color='black', linestyle='-.' , linewidth=0.8,label=f'Ajuste $g(x)$, $q = {q:.2f}$')
+        
+        xx=1   # Punto para la leyenda
+        yy=1
+        ax2.scatter(xx, yy, color='none', label=f'$\\beta={round(popt1[0],2)}$')
+    
+        
+    if f == True:
+        dataretorno = separar_par_impar(dataretorno)[0]  # El ajuste lo hago en la rama par  
+        binsretorno = np.linspace(int_ajuste[0]-0.5,int_ajuste[1]+0.5,-int_ajuste[0]+int_ajuste[1]+2)
+        #binsretorno = np.linspace(int_ajuste[0]-0.5,int_ajuste[1]+0.5,int((-int_ajuste[0]+int_ajuste[1]+2)/10))
+        y, bin_edges_tot = np.histogram(dataretorno, binsretorno)
+        x = np.linspace(int_ajuste[0],int_ajuste[1],-int_ajuste[0]+int_ajuste[1]+1).astype(int)
+        #x = np.linspace(int_ajuste[0],int_ajuste[1],int((-int_ajuste[0]+int_ajuste[1]+2)/10)-1).astype(int)
+        p00 = np.amax(y)
+        mask = y != 0   # mascara booleana que quita los puntos del histograma que valdrían cero 
+        x = x[mask]
+        y = y[mask] # (joden al calcularles el log pq log(0) es menos inf)
+        y = y/p00
+        y = np.log(y)
+                                                                              
+        # Le quitamos a x los valores entre -30,30 para que el pico donde se abre no lo tome en el ajuste
+        x_filtrado = x[(x <= int_excluido[0]) | (x >= int_excluido[1])]
+        y_filtrado = y[(x <= int_excluido[0]) | (x >= int_excluido[1])]
+        popt, pcov = curve_fit(FUNC_log, x_filtrado, y_filtrado, p0=initial_guess, bounds=([0,0], [np.inf,np.inf]))
+
+        print(label)
+        print('aq: ',round(popt[0],3), 'sin redondear: ',popt[0])
+        print('a1: ',round(popt[1],10), 'sin redondear: ',popt[1])
+
+        #x_filtrado_mayor = x_filtrado[x_filtrado >= 0]
+        #x_filtrado_menor = x_filtrado[x_filtrado <= 0]
+        #########ax2.plot(x_filtrado_mayor, 10**(FUNC_log(x_filtrado_mayor, *popt)), color='black',label=f'Ajuste $f(x)$, $q = {q:.2f}$', linewidth=0.8)
+        #########ax2.plot(x_filtrado_menor, 10**(FUNC_log(x_filtrado_menor, *popt)), color='black', linewidth=0.8)
+        #ax2.plot(x, 10**(FUNC_log(x, *popt)), color='black',label=f'Ajuste $f(x)$, $q = {q:.2f}$', linewidth=0.8)
+        
+        x_proy = np.linspace(int_graficado[0],int_graficado[1],-int_graficado[0]+int_graficado[1]+1).astype(int)
+        #x_proy_mayor = x_proy[x_proy >= x_filtrado[-1]]
+        #x_proy_menor = x_proy[x_proy <= x_filtrado[0]]
+        #ax2.plot(x_proy_mayor, 10**(FUNC_log(x_proy_mayor, *popt)), color='black', linestyle='--' , linewidth=0.8)#label='Proyección'
+        #ax2.plot(x_proy_menor, 10**(FUNC_log(x_proy_menor, *popt)), color='black', linestyle='--' , linewidth=0.8)#label='Proyección'
+        ax2.plot(x_proy, 10**(FUNC_log(x_proy, *popt)), color='black', linestyle='-' , linewidth=0.8,label=f'Ajuste $f(x)$, $q = {q:.2f}$')
+        
+        xx=1   # Punto para la leyenda
+        yy=1
+        ax2.scatter(xx, yy, color='none', label=f'$a_q={round(popt[0],3)}$\n$a_1={round(popt[1],13)}$')
+        
     #-------------------------------- 
-    xx=1   # Punto para la leyenda
-    yy=1
-    ax2.scatter(xx, yy, color='none', label=f'$a_q={round(popt[0],3)}$\n$a_1={round(popt[1],19)}$')
+    
     
     if ig == True:
-        ax2.plot(x_tot, 10**(FUNC_log(x_tot, initial_guess[0], initial_guess[1])), color='red', linewidth=0.8, linestyle='--',
+        ax2.plot(x_tot, 10**(qgaussian_log(x_tot, initial_guess)), color='red', linewidth=0.8, linestyle='--',
              label='initial guess')
     
-    
-    if qg == True:
-        print('hello')
-        ax2.plot(x_tot, qgaussian(x_tot,popt[0]), color='red', linewidth=0.8)
-
-    
+    #ax2.plot(x_proy, 10**(qgaussian_log(x_proy, 0.05)), color='red', linestyle='--' , linewidth=0.8)#label='Proyección'
     
     
     #===================== PARÁMETROS AX1
@@ -289,13 +342,13 @@ def grafico(datadelta,lim_escala,intervalo1,   dataretorno,lim_grafico,tau,int_a
     
     #fig.show()
     
-    return popt
+    return popt1, popt
 
 
-popt = grafico(datadelta,lim_escala,intervalo1,   dataretorno,lim_grafico,tau,int_ajuste,int_graficado,initial_guess,ig,qg,int_excluido,   color,marker,label,pwdgraf,nombre_archivo,save_fig)
+popt1, popt = grafico(datadelta,lim_escala,intervalo1,   dataretorno,lim_grafico,tau,int_ajusteqg,int_ajuste,int_graficado,initial_guess1,initial_guess,ig,qg,f,int_excluido,   color,marker,label,pwdgraf,nombre_archivo,save_fig)
 
 
-def grafico_zoomzoom(dataretorno,lim_graficoZOOM,popt,   color,marker,label,pwdgraf,nombre_archivo,save_fig,qg):
+def grafico_zoomzoom(dataretorno,lim_graficoZOOM,popt1,popt,   color,marker,label,pwdgraf,nombre_archivo,save_fig,qg,f):
         
     fig = plt.figure(figsize = [4.8, 3.5], layout='tight')
         
@@ -317,15 +370,14 @@ def grafico_zoomzoom(dataretorno,lim_graficoZOOM,popt,   color,marker,label,pwdg
     ax2.scatter(x_tot, 10**(y_tot), label=label, color=color, marker=marker, s=10, facecolor='none')
 
     # Grafico curva con los parámetros ajustados
-        
-              #[aq,a1]         100: [0.45278174640965785,2.7769049296420798e-05],  1000:  [-3800,3800], 5000: [], 10000: []
-    #popt = [0.45278174640965785,2.7769049296420798e-05]
-    ax2.plot(x_tot, 10**(FUNC_log(x_tot, *popt)), color='black', linewidth=0.8)
-
-
+  
     if qg == True:
-        print('hello')
-        ax2.plot(x_tot, qgaussian(x_tot,popt[0]), color='red', linewidth=0.8)
+        ax2.plot(x_tot, 10**(qgaussian_log(x_tot, popt1[0])), color='black', linestyle='-.', linewidth=0.8)
+    
+    if f == True:
+        ax2.plot(x_tot, 10**(FUNC_log(x_tot, *popt)), color='black', linewidth=0.8)
+    
+    #ax2.plot(x_tot, 10**(qgaussian_log(x_tot, 0.05)), color='red', linestyle='--' , linewidth=0.8)#label='Proyección'
    
     #=========PARAMETROS AX2================
     #ax2.set_xlim(lim_grafico[0],lim_grafico[1])
@@ -341,7 +393,7 @@ def grafico_zoomzoom(dataretorno,lim_graficoZOOM,popt,   color,marker,label,pwdg
     ax2.tick_params(labelleft=True,labelright=False)
     ax2.xaxis.set_ticks_position('both')
     ax2.tick_params(labelbottom=True,labeltop=False)
-    ax2.set_yscale("log")
+    ax2.set_yscale('log')
        
     if save_fig == True:
         #fig.savefig(pwdgraf+nombre_archivo+'ZOOMZOOM'+'.pdf', format='pdf')
@@ -349,5 +401,5 @@ def grafico_zoomzoom(dataretorno,lim_graficoZOOM,popt,   color,marker,label,pwdg
 
         #fig.show()
 
-grafico_zoomzoom(dataretorno,lim_graficoZOOM,popt,  color,marker,label,pwdgraf,nombre_archivo,save_fig,qg)  
+grafico_zoomzoom(dataretorno,lim_graficoZOOM,popt1,popt,  color,marker,label,pwdgraf,nombre_archivo,save_fig,qg,f)  
     
